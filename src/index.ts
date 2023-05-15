9a58eace9e2555d46aa06b385e11ecf0fc03e143
JamesHerman <mr.herman.j@gmail.com>
2023-05-14 22:51:15 -0400

    the walker

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,15 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application } from 'pixi.js'
+import { Scene } from './scenes/Scene';
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
+	width: 1920,
 	height: 480
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+const sceny: Scene = new Scene()
 
-clampy.anchor.set(0.5);
-
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
-
-app.stage.addChild(clampy);
+app.stage.addChild(sceny);
---
+++ src/scenes/Scene.ts
@@ -0,0 +1,64 @@
+import { AnimatedSprite, FederatedPointerEvent, Container, Texture, TextureSource, Ticker} from "pixi.js";
+
+export class Scene extends Container {
+    private animatedClampy: AnimatedSprite;
+    private clampyVelocity: number = 6;
+    constructor() {
+        super(); // Mandatory! This calls the superclass constructor.
+
+        // see how members of the class need `this.`?
+
+
+        // This is an array of strings, we need an array of Texture
+        const clampyFrames: Array<TextureSource> = [
+          "./clampies/walk_simplified_all_1.png",
+          "./clampies/walk_simplified_all_2.png",
+          "./clampies/walk_simplified_all_3.png",
+          "./clampies/walk_simplified_all_4.png"
+        ];
+        
+
+        // `array.map()` creates an array from another array by doing something to each element.
+        // `(stringy) => Texture.from(stringy)` means
+        // "A function that takes a string and returns a Texture.from(that String)"
+        this.animatedClampy = new AnimatedSprite(clampyFrames.map((frame) => Texture.from(frame)));
+        // (if this javascript is too much, you can do a simple for loop and create a new array with Texture.from())
+
+
+        this.addChild(this.animatedClampy); // we just add it to the scene
+        this.animatedClampy.loop = true;
+        this.animatedClampy.animationSpeed = .1
+        this.animatedClampy.play();
+        this.animatedClampy.scale.set(1.55)
+        this.animatedClampy.interactive = true;
+        this.animatedClampy.anchor.set(.5)
+        this.animatedClampy.y = 250
+
+        this.animatedClampy.on("pointermove", (e: FederatedPointerEvent) =>  {
+            this.clampyVelocity = (e.global.x - this.animatedClampy.x) /5
+            this.animatedClampy.scale.set(this.clampyVelocity > 0 ? 1.55 : -1.55,1.55)
+        })
+        
+        Ticker.shared.add(this.update, this);
+
+        // Now... what did we learn about assigning functions...
+        this.animatedClampy.onFrameChange = this.onClampyFrameChange.bind(this);
+    }
+
+    private update(deltaTime: number): void {
+        this.animatedClampy.x = this.animatedClampy.x + this.clampyVelocity * deltaTime;
+
+        if (this.animatedClampy.x > 2200) {
+            // Woah there clampy, come back inside the screen!
+            this.animatedClampy.x = -300;
+        }
+        if (this.animatedClampy.x < -300) {
+            // Woah there clampy, come back inside the screen!
+            this.animatedClampy.x = 2200;
+        }
+    }
+
+    private onClampyFrameChange(currentFrame: any): void {
+        console.log("Clampy's current frame is", currentFrame);
+    }
+}
